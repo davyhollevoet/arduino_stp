865cfe338c0545f77d8a34a41893c3ca7a3ceb34
Davy Hollevoet <davy@natox.be>
2020-04-25 11:42:39 +0200

    toggle high speed mode when user presses all buttons

--- examples/stp_abs_w/stp_abs_w.ino
+++ examples/stp_abs_w/stp_abs_w.ino
@@ -5,9 +5,16 @@
 PS2 ps2dev(6, 5);
 STP stp(ps2dev);
 
+uint8_t all_ext_buttons;
+
 void setup() {
     Serial.begin(57600);
     stp.init(STP_MODE_ABS_W);
+
+    //if state.ext == all_ext_buttons, then the user is pressing all the buttons
+    all_ext_buttons = (1u << stp.n_extended_buttons()) - 1;
+
+    //setup names for the Serial Plotter
     Serial.println("x\ty\tLeft\tRight\tMiddle\tExt");
 }
 
@@ -28,4 +35,19 @@ void loop() {
     Serial.print("\t");
     Serial.print(state.ext, DEC);
     Serial.println();
+
+    //switch to high rate mode and back when the user presses all extended buttons
+    //don't do this if there are no buttons
+    static bool pressed = false;
+    static bool high_rate = false;
+
+    if (all_ext_buttons && state.ext == all_ext_buttons) {
+        pressed = true;
+    } else if (pressed) {
+        pressed = false;
+        high_rate = !high_rate;
+        stp.disable();
+        stp.set_byte_mode(high_rate ? STP_MODE_ABS_W_HI_RATE : STP_MODE_ABS_W);
+        stp.enable();
+    }
 }
